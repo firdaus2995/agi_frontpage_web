fd82a7ab6ec3ccabaa69696733ac761c594eba86
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-06-05 16:15:33 +0700

    ACW2-3057: Slicing & Responsive Agency Modul 4 AGI

--- src/components/molecules/specifics/agi/Cards/CategoriB/index.tsx
+++ src/components/molecules/specifics/agi/Cards/CategoriB/index.tsx
@@ -6,24 +6,35 @@ import BlankImage from '@/assets/images/blank-image.svg';
 interface ICardCategoryB {
   summary: string;
   description: string;
+  imageUrl?: string;
+  lineClamp?: number;
+  imageStyle?: string;
 }
 
 const CardCategoryB = ({
   summary,
   description,
+  imageUrl,
+  imageStyle,
+  lineClamp
 }: ICardCategoryB) => {
   return (
-    <div className="flex flex-col gap-[18px] border border-gray_light border-b-8 border-b-purple_dark rounded-[12px] text-left">
+    <div className="flex flex-col gap-[18px] border border-gray_light border-b-8 border-b-purple_dark rounded-[12px] text-left h-full">
       <Image
         alt="blank-image"
         width={0}
         height={170}
-        src={BlankImage}
-        className="w-auto rounded-t-[12px]"
+        src={imageUrl ?? BlankImage}
+        className={`w-auto rounded-t-[12px] ${imageStyle ?? ''}`}
       />
-      <div className='flex flex-col gap-4 p-5'>
-        <p className="text-[20px] font-bold">{summary}</p>
-        <p className='text-xs'>{description}</p>
+      <div className="h-full flex flex-col justify-between px-5 pb-5">
+        <p
+          className={`text-[20px] font-bold font-karla line-clamp-3 ${lineClamp?.toString() ?? ''}`}
+          dangerouslySetInnerHTML={{
+            __html: summary
+          }}
+        />
+        <p className="text-xs mt-2 font-opensans">{description}</p>
       </div>
     </div>
   );
--- src/components/molecules/specifics/agi/PusatInformasi/Agency/index.tsx
+++ src/components/molecules/specifics/agi/PusatInformasi/Agency/index.tsx
@@ -12,31 +12,26 @@ const Agency = () => {
 
   return (
     <div
-      className={`flex flex-col gap-4 mb-10 ${tab.includes('Agency') && content.includes('detail') ? 'mx-[32px] md:mx-[136px]' : ''}`}
+      className={`flex flex-col gap-4 ${tab.includes('Agency') && content.includes('detail') ? '' : 'px-[2rem] md:px-[8.5rem] pb-[3.25rem]'}`}
     >
-      {tab.includes('Agency') && content.includes('detail') ? (
-        <>
-          <Detail />
-          <div
-            className={`flex xs:flex-col md:flex-row items-center gap-4 justify-between border border-gray_light rounded-xl p-4`}
-          >
-            <div className="flex flex-row gap-4 items-center">
-              <h1 className="font-bold text-xl 2xl:text-2xl text-purple_dark">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-              </h1>
-            </div>
-            <div className="xs:w-full md:w-auto">
-              <Button
-                title="Agency Management System"
-                customButtonClass="bg-purple_dark rounded-lg"
-                customTextClass="text-white"
-              />
-            </div>
-          </div>
-        </>
-      ) : (
-        <Content />
-      )}
+      {tab.includes('Agency') && content ? <Detail /> : <Content />}
+
+      <div
+        className={`flex xs:flex-col md:flex-row items-center gap-4 justify-between border border-gray_light rounded-xl p-4 ${tab.includes('Agency') && content.includes('detail') ? 'mx-[2rem] md:mx-[8.5rem] mb-[3.25rem]' : ''}`}
+      >
+        <div className="flex flex-row gap-4 items-center">
+          <h1 className="font-bold text-xl 2xl:text-2xl text-purple_dark">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+          </h1>
+        </div>
+        <div className="xs:w-full md:w-auto">
+          <Button
+            title="Agency Management System"
+            customButtonClass="bg-purple_dark rounded-lg"
+            customTextClass="text-white"
+          />
+        </div>
+      </div>
     </div>
   );
 };

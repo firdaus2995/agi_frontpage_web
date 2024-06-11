e5754cfdc5feab180c41ee172c09198eef25a696
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-11 21:51:36 +0700

    ACW2-2980: Slicing : Modul 1 - Halaman Pencarian

--- src/app/pencarian/page.tsx
+++ src/app/pencarian/page.tsx
@@ -1,60 +1,85 @@
-import React from 'react';
-import CUSTOMER_SERVICE from '@/assets/images/common/customer-service.svg';
-import DOCUMENT_SEARCH from '@/assets/images/common/document-search.svg';
-import EMAIL from '@/assets/images/common/email.svg';
-import MESSAGE from '@/assets/images/common/message.svg';
-import GROUP_PHOTO from '@/assets/images/group-photo.svg';
+'use client';
+import React, { useEffect, useState } from 'react';
 import RoundedFrameBottom from '@/components/atoms/RoundedFrameBottom';
 import RoundedFrameTop from '@/components/atoms/RoundedFrameTop';
 import FooterCards from '@/components/molecules/specifics/agi/FooterCards';
 import FooterInformation from '@/components/molecules/specifics/agi/FooterInformation';
 import Hero from '@/components/molecules/specifics/agi/Hero';
 import SearchForm from '@/components/molecules/specifics/agi/Pencarian/SearchForm';
-
-const breadcrumbsData = [
-  { title: 'Beranda', href: '/' },
-  { title: 'Pencarian', href: '/pencarian' }
-];
+import { handleGetContentPage } from '@/services/content-page.api';
+import { contentStringTransformer, pageTransformer, singleImageTransformer } from '@/utils/responseTransformer';
 
 const Pencarian = () => {
+  const [title, setTitle] = useState('');
+  const [titleImg, setTitleImg] = useState({ imageUrl: '', altText: '' });
+
+  const [cta1Img, setCta1Img] = useState({ imageUrl: '', altText: '' });
+  const [cta1Name, setCta1Name] = useState('');
+  const [cta1Label, setCta1Label] = useState('');
+  const [cta1Link, setCta1Link] = useState('');
+
+  const [listBanner, setListBanner] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await handleGetContentPage('hal-pencarian-agi');
+        // page
+        const { content } = pageTransformer(data);
+        setTitleImg(singleImageTransformer(content['title-image']));
+        setTitle(contentStringTransformer(content['title-judul']));
+
+        setCta1Img(singleImageTransformer(content['cta1-image']));
+        setCta1Name(contentStringTransformer(content['cta1-teks']));
+        setCta1Label(contentStringTransformer(content['cta1-label-button']));
+        setCta1Link(contentStringTransformer(content['cta1-link-button']));
+
+        const cta4Data =[];
+        for (let i = 0; i < 4; i++) {
+          const icon = singleImageTransformer(content[`cta4-${i + 1}-icon`]);
+          const title = contentStringTransformer(content[`cta4-${i + 1}-nama`]);
+          const subtitle = contentStringTransformer(content[`cta4-${i + 1}-label-link`]);
+          const href = contentStringTransformer(content[`cta4-${i + 1}-link`]);
+        
+          if (icon && title && subtitle) {
+            cta4Data.push({
+              icon: icon.imageUrl,
+              title: title,
+              subtitle: subtitle,
+              href: href
+            });
+          }
+        }
+
+        setListBanner(cta4Data)
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const breadcrumbsData = [
+    { title: 'Beranda', href: '/' },
+    { title: title, href: '/pencarian' }
+  ];
   return (
     <div className="flex flex-col">
-      <Hero title="Pencarian" breadcrumbsData={breadcrumbsData} />
+      <Hero title={title} breadcrumbsData={breadcrumbsData} imageUrl={titleImg.imageUrl} />
       <SearchForm />
       <RoundedFrameBottom frameColor="bg-white" />
       <FooterInformation
         title={
-          <p className="text-[48px]">
-            Ada yang bisa <span className="text-purple_dark font-bold">AvGen</span> bantu untuk Anda?
-          </p>
+          <p className="text-[48px]" dangerouslySetInnerHTML={{ __html: cta1Name ?? '' }} />
         }
-        buttonTitle="Tanya AvGen"
-        image={GROUP_PHOTO}
+        buttonTitle={cta1Label}
+        href={cta1Link}
+        image={cta1Img.imageUrl}
       />
       <RoundedFrameTop />
       <FooterCards
-        cards={[
-          {
-            title: 'Layanan Nasabah',
-            subtitle: '021 5789 8188',
-            icon: CUSTOMER_SERVICE
-          },
-          {
-            title: 'Tanya Avrista',
-            subtitle: 'Lebih Lanjut',
-            icon: MESSAGE
-          },
-          {
-            title: 'Tanya Lewat Email',
-            subtitle: 'Kirim Email',
-            icon: EMAIL
-          },
-          {
-            title: 'Prosedur Pengaduan',
-            subtitle: 'Lihat Prosedur',
-            icon: DOCUMENT_SEARCH
-          }
-        ]}
+        cards={listBanner}
       />
     </div>
   );

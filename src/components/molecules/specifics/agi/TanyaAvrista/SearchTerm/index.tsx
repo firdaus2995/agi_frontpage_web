42fe9c7526046fec3929817440b05f811d505229
Firdaus <muhamad.firdaus2995@gmail.com>
2024-07-18 01:18:18 +0700

    ACW2-2981: Slicing : Modul 1 - Halaman Tanya Avgen

--- src/components/molecules/specifics/agi/TanyaAvrista/SearchTerm/index.tsx
+++ src/components/molecules/specifics/agi/TanyaAvrista/SearchTerm/index.tsx
@@ -6,17 +6,19 @@ import Input from '@/components/atoms/Input';
 
 const SearchTerm = ({ bannerImage }: { bannerImage: string }) => {
   return (
-    <div className="z-10 w-full xs:-mt-[3.4rem] md:-mt-[6.3rem]">
-      <div className="w-full h-[640px] flex items-center absolute">
-        <div className="w-full h-full grid grid-cols-2">
+    <div className="z-[1] w-full bg-purple_dark -mt-[3.125rem]">
+      <div className="w-full h-[20rem] md:h-[40rem] flex items-center absolute">
+        <div className="w-full h-full grid sm:grid-cols-2 xs:grid-cols-1">
           <span />
-          <div className="flex flex-col md:justify-center sm:gap-1 md:gap-4 md:ml-20 sm:ml-0">
-            <p className="font-karla text-banner-footer-2-desktop font-bold text-white">
-              Halo, apa kabar?
-            </p>
-            <p className="font-karla text-banner-footer-2-desktop text-white">
-              Ada yang AvGen bisa bantu?
-            </p>
+          <div className="flex flex-col xs:justify-center sm:gap-1 xs:gap-[36px] xs:px-[36px] sm:px-0 sm:pb-0 xs:pb-[4rem]">
+            <div>
+              <p className="font-karla text-[20px] lg:text-[48px] font-extrabold text-white text-shadow-h1 sm:leading-[57.6px] tracking-[0.03em]">
+                Halo, apa kabar?
+              </p>
+              <p className="font-karla text-[20px] lg:text-[48px] text-white text-shadow-h1 sm:leading-[57.6px] tracking-[0.03em]">
+                Ada yang AvGen bisa bantu?
+              </p>
+            </div>
             <div className="w-full sm:mt-2 mt-6">
               <Input
                 customInputClass="md:w-[60%] 2xl:w-[72%] grow !bg-gray_bglightgray !border-none"
@@ -26,13 +28,15 @@ const SearchTerm = ({ bannerImage }: { bannerImage: string }) => {
           </div>
         </div>
       </div>
-      <Image
-        className="rounded-t-[65px] w-full object-cover h-[640px]"
-        alt="gambar-produk-individu"
-        src={bannerImage}
-        width={0}
-        height={0}
-      />
+      {bannerImage !== '' && (
+        <Image
+          className="w-full sm:object-cover xs:object-none xs:object-right sm:object-center h-[640px]"
+          alt="gambar-produk-individu"
+          src={bannerImage}
+          width={0}
+          height={0}
+        />
+      )}
     </div>
   );
 };

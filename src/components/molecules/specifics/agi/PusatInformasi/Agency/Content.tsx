de3b5e65fd59728b88d03e2276facbdcbe2e6e3e
Firdaus <muhamad.firdaus2995@gmail.com>
2024-09-10 20:43:56 +0700

    ACW2-4928: Bugfix title halaman agency

--- src/components/molecules/specifics/agi/PusatInformasi/Agency/Content.tsx
+++ src/components/molecules/specifics/agi/PusatInformasi/Agency/Content.tsx
@@ -311,7 +311,7 @@ const Content = (props: contentProps) => {
   return (
     <div className="w-full flex flex-col items-center justify-center py-2 pt-[5rem] pb-[32px] lg:pb-[64px]">
       <div className="text-center lg:pb-2 xs:pb-[56px]">
-        <h2 className="text-[2.25rem] 2xl:text-[3.5rem] font-medium text-purple_dark">
+        <h2 className="text-[2.25rem] 2xl:text-[3.5rem] font-bold text-purple_dark">
           {contentStringTransformer(pageData['nama-section'])}
         </h2>
         <h2

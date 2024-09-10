27840c096601b9a8b516e3fe7b56e84fd01b7caa
Firdaus <muhamad.firdaus2995@gmail.com>
2024-09-10 20:37:51 +0700

    ACW2-4925: Bugfix title halaman klaim

--- src/components/molecules/specifics/agi/PusatInformasi/Klaim/index.tsx
+++ src/components/molecules/specifics/agi/PusatInformasi/Klaim/index.tsx
@@ -23,9 +23,9 @@ const Klaim = () => {
   return (
     <div className="flex flex-col px-[2rem] lg:px-[8.5rem] pb-[6.25rem]">
       <section className="w-full flex flex-col items-center text-center py-[80px]">
-        <h1 className="font-karla font-light text-[2.25rem] lg:text-[3.5rem] leading-[120%] -tracking-[0.04em]">
-          <span className="text-purple_dark font-bold">Transparansi</span> dan{' '}
-          <span className="text-purple_dark font-bold">efisiensi</span> dalam
+        <h1 className="font-karla font-bold text-[2.25rem] lg:text-[3.5rem] leading-[120%] -tracking-[0.04em]">
+          <span className="text-purple_dark">Transparansi</span> dan{' '}
+          <span className="text-purple_dark">efisiensi</span> dalam
           menangani permohonan klaim nasabah.
         </h1>
       </section>
